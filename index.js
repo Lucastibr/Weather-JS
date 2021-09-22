38aae6597b3ddbadb616725d6dcaff196a1d952d
Lucas <lucas@codout.com>
2021-09-21 21:56:22 -0300

    Ajustes layout e correção bugs

--- index.js
+++ index.js
@@ -3,90 +3,95 @@ const apiKey = "";
 // Here's your APIKEY
 
 window.addEventListener('load', () => {
-    navigator.geolocation.getCurrentPosition((position) => {
-        console.log(Math.abs(position.coords.latitude), Math.abs(position.coords.longitude));
+ navigator.geolocation.getCurrentPosition((position) => {
+    console.log(Math.abs(position.coords.latitude), Math.abs(position.coords.longitude));
 
-        latitude = Math.abs(position.coords.latitude);
-        longitude = Math.abs(position.coords.longitude);
-      
-        openWeatherApiCoordinates(latitude, longitude);
-      });
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+
+    openWeatherApiCoordinates(latitude, longitude);
+  });  
 });
 
 let latitude, longitude;
 
 const openWeatherApi = (query) => {
 
-    if(query.length <= 0 || query === "") {
+  if (query.length <= 0 || query === "") {
+    errorLocal();
+  }
+
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${query}&lang=pt_br&units=metric&APPID=${apiKey}`;
+
+  const title = document.getElementById('title');
+  const details = document.getElementById('details');
+  const otherDetails = document.getElementById('otherDetails');
+  const description = document.getElementById('description');
+  const image = document.getElementById('image');
+
+  fetch(url).then(async (response) => {
+    var contentType = response.headers.get("content-type");
+    if (contentType && contentType.indexOf("application/json") !== -1) {
+      const json = await response.json();
+      if (response.status === 404) {
+        return Swal.fire('Oooops!', 'Cidade não encontrada!', 'warning');
+      }
+      title.innerHTML = json.name;
+      details.innerHTML = `${parseInt(json.main.temp)}º`;
+      otherDetails.innerHTML = `Sensação Térmica : ${parseInt(json.main.feels_like)}º `;
+      json.weather.forEach(weather => {
+        const img = `${weather.icon}@2x.png`
+        description.innerHTML = weather.description.toUpperCase();
+      })
+      console.log(json);
+    } else {
       errorLocal();
     }
-
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&APPID=${apiKey}`;
-
-    const title = document.getElementById('title');
-    const details = document.getElementById('details');
-    const otherDetails = document.getElementById('otherDetails');
-
-    fetch(url).then(async (response) => {
-        var contentType = response.headers.get("content-type");
-        if(contentType && contentType.indexOf("application/json") !== -1) {
-          const json = await response.json();
-          if(response.status === 404){
-            return Swal.fire('Oooops!', 'Cidade não encontrada!', 'warning');
-          }
-            title.innerHTML = json.name;
-            details.innerHTML = `${parseInt(json.main.temp)}º`;
-            otherDetails.innerHTML = `Sensação Térmica:${parseInt(json.main.feels_like)}º`
-            console.log(json);
-        } else {
-          errorLocal();
-        }
-      })
-      .catch(function(error) {
-          if(error.message.includes("temp") || error.code === 404 ) {
-            return Swal.fire('Oooops!', 'Cidade não encontrada!', 'warning');
-          }
-          errorLocal();
-      });
+  })
+    .catch(function (error) {
+      if (error.message.includes("temp") || error.code === 404) {
+        return Swal.fire('Oooops!', 'Cidade não encontrada!', 'warning');
+      }
+      errorLocal();
+    });
 }
 
 const openModal = () => {
-    var modal = new bootstrap.Modal(document.getElementById('newSearch'), {
-        keyboard: false
-      });
+  var modal = new bootstrap.Modal(document.getElementById('newSearch'), {
+    keyboard: false
+  });
 
-      modal.show();
+  modal.show();
 
 }
 
-const openWeatherApiCoordinates = (lat, lon) => {   
+const openWeatherApiCoordinates = (lat, lon) => {
 
-    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=pt_br&&units=metric&appid=${apiKey}`;
 
-    const title = document.getElementById('title');
-    const details = document.getElementById('details');
-    const otherDetails = document.getElementById('otherDetails');
+  const title = document.getElementById('title');
+  const details = document.getElementById('details');
+  const otherDetails = document.getElementById('otherDetails');
 
-    fetch(url).then(async (response) => {
+  fetch(url).then(async (response) => {
+    console.log(url);
+    if (response.status === 401) {
+      return Swal.fire('Oooops!', 'Você não adicionou uma APIKEY válida!', 'warning')
+    }
 
-       if(response.status === 401){
-         return Swal.fire('Oooops!','Você não adicionou uma APIKEY válida!', 'warning')
-       }
-
-        var contentType = response.headers.get("content-type");
-        if(contentType && contentType.indexOf("application/json") !== -1) {
-          const json = await response.json();
-            title.innerHTML = json.name;
-            details.innerHTML = `${parseInt(json.main.temp)}º`;
-            otherDetails.innerHTML = `Sensação Térmica:${parseInt(json.main.feels_like)}º`
-            console.log(json);
-        } else {
-            return Swal.fire('Oooops!', 'Você não digitou nenhum local!', 'warning');
-        }
-      })
-      .catch(function(error) {
-        return Swal.fire('Oooops!', error.message, 'warning');
-      });
+    var contentType = response.headers.get("content-type");
+    if (contentType && contentType.indexOf("application/json") !== -1) {
+      const json = await response.json();
+      title.innerHTML = json.name;
+      details.innerHTML = `${parseInt(json.main.temp)}º`;
+      otherDetails.innerHTML = `Sensação Térmica:${parseInt(json.main.feels_like)}º`;
+    } else {
+      return Swal.fire('Oooops!', 'Você não digitou nenhum local!', 'warning');
+    }
+  })
+    .catch(function (error) {
+      return Swal.fire('Oooops!', error.message, 'warning');
+    });
 }
 
 const errorLocal = () => {
