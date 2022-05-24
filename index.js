8ca1f9b0d7fdb8552608a083dae4aa7b99727df9
Lucas <lucas@codout.com>
2022-05-24 13:06:32 -0300

    Code Refactoring

--- index.js
+++ index.js
@@ -4,24 +4,30 @@ const apiKey = "";
 
 window.addEventListener('load', () => {
  navigator.geolocation.getCurrentPosition((position) => {
-    console.log(Math.abs(position.coords.latitude), Math.abs(position.coords.longitude));
-
     latitude = position.coords.latitude;
     longitude = position.coords.longitude;
 
-    openWeatherApiCoordinates(latitude, longitude);
+    openWeatherApi(null, latitude, longitude);
   });  
 });
 
 let latitude, longitude;
 
-const openWeatherApi = (query) => {
+const openWeatherApi = (query, lat, lon) => {
+  
+  let url = "";
 
-  if (query.length <= 0 || query === "") {
+  if(query != null && (query.length <= 0 || query === "")) {
     errorLocal();
+    return;
   }
 
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${query}&lang=pt_br&units=metric&APPID=${apiKey}`;
+  if(query === null){
+    url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=pt_br&&units=metric&appid=${apiKey}`;
+  }
+  else{
+    url = `http://api.openweathermap.org/data/2.5/weather?q=${query}&lang=pt_br&units=metric&APPID=${apiKey}`;
+  }
 
   const title = document.getElementById('title');
   const details = document.getElementById('details');
@@ -40,10 +46,10 @@ const openWeatherApi = (query) => {
       details.innerHTML = `${parseInt(json.main.temp)}º`;
       otherDetails.innerHTML = `Sensação Térmica : ${parseInt(json.main.feels_like)}º `;
       json.weather.forEach(weather => {
+        console.log(weather);
         const img = `${weather.icon}@2x.png`
         description.innerHTML = weather.description.toUpperCase();
       })
-      console.log(json);
     } else {
       errorLocal();
     }
@@ -65,38 +71,7 @@ const openModal = () => {
 
 }
 
-const openWeatherApiCoordinates = (lat, lon) => {
-
-  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=pt_br&&units=metric&appid=${apiKey}`;
-
-  const title = document.getElementById('title');
-  const details = document.getElementById('details');
-  const otherDetails = document.getElementById('otherDetails');
-
-  fetch(url).then(async (response) => {
-    console.log(url);
-    if (response.status === 401) {
-      return Swal.fire('Oooops!', 'Você não adicionou uma APIKEY válida!', 'warning')
-    }
-
-    var contentType = response.headers.get("content-type");
-    if (contentType && contentType.indexOf("application/json") !== -1) {
-      const json = await response.json();
-      title.innerHTML = json.name;
-      details.innerHTML = `${parseInt(json.main.temp)}º`;
-      otherDetails.innerHTML = `Sensação Térmica:${parseInt(json.main.feels_like)}º`;
-    } else {
-      return Swal.fire('Oooops!', 'Você não digitou nenhum local!', 'warning');
-    }
-  })
-    .catch(function (error) {
-      return Swal.fire('Oooops!', error.message, 'warning');
-    });
-}
-
-const errorLocal = () => {
-  return Swal.fire('Oooops!', 'Você não digitou nenhum local!', 'warning');
-}
+const errorLocal = () => Swal.fire('Oooops!', 'Você não digitou nenhuma cidade!', 'warning');
 
 
 
